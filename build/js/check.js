6e317fd18a88ea15b2d2caa7496183206ecec96b
aleksi4 <alexandernikonov88@gmail.com>
2016-05-27 22:08:01 +0300

    убрал лишние скобки, изменил проверку на массив

--- build/js/check.js
+++ build/js/check.js
@@ -1,26 +1,26 @@
 var getMessage = function (a, b) {
-  if (typeof(a) === "boolean") {
-    if (a === true) {
+  if (typeof a === "boolean") {
+    if (a) {
       return "Я попал в " + b;
     }
     else {
       return "Я никуда не попал";
     }
   }
-  
-  else if (typeof(a) === "number") {
+
+  else if (typeof a === "number") {
     return "Я прыгнул на " + (a * 100) + " сантиметров";
   }
-  
-  else if (typeof(a) === "object") {
+
+  else if (a instanceof Array) {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       sum += a[i];
     }
     return "Я прошёл " + sum + " шагов";
   }
-  
-  else if (typeof(a) === "object" && typeof(b) === "object") {
+
+  else if (a instanceof Array && b instanceof Array) {
     var length = 0;
     for (var i = 0; i < a.length; i++) {
       length += a[i] * b[i];
